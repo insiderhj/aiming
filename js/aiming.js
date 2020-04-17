4d53a3b45102f7447fe98a0b6bbe75c177dea4cd
insiderhj <insiderhj@gmail.com>
2020-04-17 18:56:21 +0900

    원이 움직인다!

--- js/aiming.js
+++ js/aiming.js
@@ -1,14 +1,18 @@
 function changePos() {
-    var xPos = Math.floor(Math.random() * 700);
-    var yPos = Math.floor(Math.random() * 400);
-    //TODO
+    var xPos = Math.floor(Math.random() * (700 - $('#circle').width()));
+    var yPos = Math.floor(Math.random() * (400 - $('#circle').height()));
+    $('#circle').css('left', xPos);
+    $('#circle').css('top', yPos);
 }
 
 $(document).ready(function() {
     changePos();
-});
-
-$('#circle').click(function() {
-    $('#cur_score').text(Number($('#cur_score').text()) + 1);
-    changePos();
+    
+    $('#circle').click(function() {
+        $('#cur_score').text(Number($('#cur_score').text()) + 1);
+        $('#circle').css('width', $('#circle').width() * 0.98);
+        changePos();
+    });
+    
+    $(".timer pre").text("30");
 });
